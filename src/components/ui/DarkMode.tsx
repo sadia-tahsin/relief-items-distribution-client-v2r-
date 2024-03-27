9f50b4e5eba34ed461e9a499975becfe42e35fb6
sadia-tahsin <sadia-tahsin3.1415@gmail.com>
2024-03-27 19:38:03 +0600

    deployed

--- src/components/ui/DarkMode.tsx
+++ src/components/ui/DarkMode.tsx
@@ -1,26 +1,26 @@
-import { createContext, useContext, useState } from 'react';
+// import { createContext, useContext, useState } from 'react';
 
-interface DarkModeContextType {
-  isDarkMode: boolean;
-  setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
-}
+// interface DarkModeContextType {
+//   isDarkMode: boolean;
+//   setIsDarkMode: React.Dispatch<React.SetStateAction<boolean>>;
+// }
 
-const DarkModeContext = createContext<DarkModeContextType | undefined>(undefined);
+// const DarkModeContext = createContext<DarkModeContextType | undefined>(undefined);
 
-export const DarkModeProvider: React.FC = ({ children }:DarkModeContextType) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+// export const DarkModeProvider: React.FC = ({ children }:DarkModeContextType) => {
+//   const [isDarkMode, setIsDarkMode] = useState(false);
 
-  return (
-    <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
-      {children}
-    </DarkModeContext.Provider>
-  );
-};
+//   return (
+//     <DarkModeContext.Provider value={{ isDarkMode, setIsDarkMode }}>
+//       {children}
+//     </DarkModeContext.Provider>
+//   );
+// };
 
-export const useDarkMode = (): DarkModeContextType => {
-  const context = useContext(DarkModeContext);
-  if (!context) {
-    throw new Error('useDarkMode must be used within a DarkModeProvider');
-  }
-  return context;
-};
+// export const useDarkMode = (): DarkModeContextType => {
+//   const context = useContext(DarkModeContext);
+//   if (!context) {
+//     throw new Error('useDarkMode must be used within a DarkModeProvider');
+//   }
+//   return context;
+// };
